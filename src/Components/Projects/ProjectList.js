51c9633f8486cdeb5f690afd7a3e93c8fa3e5300
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-02-01 22:21:18 -0500

    Add normalize.css

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -1,6 +1,7 @@
 // Components
 import Project from './Project';
 // Media
+import roggSmartLink from './img/rogg-smartlink.jpg'
 import battleTracker from './img/battle-tracker.jpg';
 
 const ProjectList = () => {
@@ -8,7 +9,7 @@ const ProjectList = () => {
         <>
             <Project
                 title="This is a project"
-                image={battleTracker}
+                image={roggSmartLink}
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris sit amet. Leo urna molestie at elementum eu facilisis sed odio. Purus in mollis nunc sed id semper risus in. Eget felis eget nunc lobortis mattis. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris sit amet. Leo urna molestie at elementum eu facilisis sed odio. Purus in mollis nunc sed id semper risus in. Eget felis eget nunc lobortis mattis. Bibendum est ultricies integer quis auctor elit sed vulputate mi."
                 codeLink='https://github.com'
             />
