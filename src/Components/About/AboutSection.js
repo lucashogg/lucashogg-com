5c91ca72aa330562af8ebc136b11ad3e0decf34b
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-02-05 14:59:20 -0500

    Add content to About page

---
+++ src/Components/About/AboutSection.js
@@ -0,0 +1,30 @@
+// CSS
+import './AboutSection.css';
+// Components
+import CustomLink from '../Utlility/CustomLink';
+// Media
+import avatar from './img/lucas.jpg';
+
+const AboutSection = () => {
+    return (
+        <>
+            <div className="about">
+                <img src={avatar} alt="Lucas Hogg" />
+                <div className="about-text">
+                    <h2>Hey there</h2>
+                    <p>
+                        My name is Lucas Hogg. I currently specialize in front-end modification for a proprietary SaaS platform that provides e-commerce-based storefronts to clients. Self-taught and a self-starter, I've always enjoyed exploring new technologies; all starting with a background in design and video/music production.
+                    </p>
+                    <p>
+                        Formerly a full-time coffee roaster, I've been able to leverage the available online recourses for self study in the field of computer science to switch career paths. I'm super passionate about creating experiences that aren't just made, but also shared.
+                    </p>
+                    <p>
+                        I also produce music as a hobby, listen to that <CustomLink href="https://lucashogg.killtheheartache.com" target="_blank" name="here" />.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AboutSection
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,9 @@
+// Components
+import AboutSection from '../Components/About/AboutSection';
+
 const About = () => {
     return (
-        <>
-            <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean vel elit. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Mattis molestie a iaculis at erat pellentesque. Ac tortor dignissim convallis aenean et. Cursus eget nunc scelerisque viverra mauris. Aliquet eget sit amet tellus cras adipiscing. Mollis nunc sed id semper. Sed blandit libero volutpat sed cras ornare arcu. Orci dapibus ultrices in iaculis nunc sed augue lacus viverra. Gravida in fermentum et sollicitudin ac orci phasellus egestas tellus. Feugiat in fermentum posuere urna.</p>
-        </>
+        <AboutSection />
     )
 }
 
