805b9bea2754902ff6c8186b02d2e88a429113b6
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-02-16 14:19:15 -0500

    Add image

--- src/Components/ArWorkEx/ArWorkList.js
+++ src/Components/ArWorkEx/ArWorkList.js
@@ -1,8 +1,12 @@
 // Components
 import ArWork from "./ArWork";
+// Media
+import blankStorefront from './img/blank-storefront.jpg';
 
 const ArWorkList = () => {
-    return <ArWork />
+    return <ArWork 
+        image={blankStorefront}
+    />
 }
 
 export default ArWorkList
