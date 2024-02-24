614f8c685e03b0d37db365ad193df2fd5186a437
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-02-24 11:21:00 -0500

    Update verbiage

--- src/Components/About/AboutSection.js
+++ src/Components/About/AboutSection.js
@@ -16,7 +16,7 @@ const AboutSection = () => {
                         My name is Lucas Hogg. I currently specialize in customizing and enhancing the user experience for a proprietary SaaS platform that provides e-commerce-based storefronts to clients. I use my skills in responsive web design, JavaScript, HTML/CSS, and graphic design to create personalized and engaging websites that showcase our clients' products and services.
                     </p>
                     <p>
-                        I have been working as a front-end developer for over two years, after switching careers from coffee roasting. I am a self-taught and self-motivated learner who constantly explores new technologies and tools. I'm passionate about creating experiences that aren't just made, but also shared.
+                        I have been working in web development for over two years, after switching careers from coffee roasting. I am a self-taught and self-motivated learner who constantly explores new technologies and tools. I'm passionate about creating experiences that aren't just made, but also shared.
                     </p>
                     <p>
                         I also produce music as a hobby. Listen to that <CustomLink href="https://lucashogg.killtheheartache.com" target="_blank" name="here." />
